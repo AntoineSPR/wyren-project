ef203816b162e6a10b49a4826075a68cca5da3a0
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-20 17:31:52 +0100

    Add files via upload
    Pris en compte le temps de chargement de l'image et affiché un message en conséquence

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -1,15 +1,23 @@
 const overlay = document.getElementById("overlay");
 const conteneurImage = document.getElementById("conteneurImage");
 const image = document.getElementById("image");
+const chargement = document.getElementById("chargement");
 
-function ouvrirOverlay(url) {
-    image.src = url;
+function ouvrirOverlay(url) {   
     overlay.style.display = "block";
     conteneurImage.style.display = "block";
+    chargement.style.display = "block";
+
+    image.onload = () => {
+        chargement.style.display = "none";
+        console.log("chargement en cours")
+    }
+    image.src = url;
 }
 
 function fermerOverlay() {
     overlay.style.display = "none";
     conteneurImage.style.display = "none";
     image.src = "";
+    chargement.style.display = "none";
 }

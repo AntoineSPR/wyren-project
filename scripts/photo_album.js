c6bcac0d799885ceee2512b654783866b546f171
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-22 22:30:24 +0100

    Add files via upload
    Ajout d'une fonction pour les appareils plus lents : disparition du message de chargement lorsque l'image a atteint la moitié de sa hauteur (pour éviter la surimpression)

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -3,6 +3,7 @@ const imageContainer = document.getElementById("image-container");
 const image = document.getElementById("image");
 const loading = document.getElementById("loading");
 
+//Fonction d'affichage de l'overlay et de la photo en grande taille//
 function openOverlay(url) {   
     overlay.style.display = "block";
     imageContainer.style.display = "block";
@@ -10,9 +11,19 @@ function openOverlay(url) {
 
     image.src = url;
 
+    //Disparition du message de chargement dès que l'image est chargée//
     image.onload = () => {
         loading.style.display = "none";
     }
+    //Pour les appareils plus lents, vérification toutes les 0.1 secondes si l'image s'est chargée à plus de 45%//
+    let checkInterval = setInterval(
+        () => {
+            if (image.height >= image.height * 0.45){
+                loading.style.display = "none";
+                clearInterval(checkInterval);
+            } 
+        }
+    , 100);
 }
 
 function hideOverlay() {

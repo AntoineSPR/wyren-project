70cd593d9dc4bbab91f10e371b694e4933ca84bf
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-25 22:19:45 +0100

    Add files via upload
    Suppression du message de chargement (superflu au vu de l'affichage des boutons)

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -6,28 +6,12 @@ const fullImage = document.getElementById("image");
 
 let currentImage = 0;
 
-//Fonction d'affichage de l'overlay et de la photo en grande taille//
 function openOverlay(image) {   
     overlay.style.display = "block";
     imageContainer.style.display = "block";
-    loading.style.display = "block";
 
     fullImage.src = image.src.replace("-sq.jpg", ".jpg");
     currentImage = Number(image.id);
-
-    //Disparition du message de chargement dès que l'image est chargée//
-    fullImage.onload = () => {
-        loading.style.display = "none";
-    }
-    //Pour les appareils plus lents, vérification toutes les 0.1 secondes si l'image s'est chargée à plus de 45%//
-    let checkInterval = setInterval(
-        () => {
-            if (fullImage.height >= fullImage.height * 0.45){
-                loading.style.display = "none";
-                clearInterval(checkInterval);
-            } 
-        }
-    , 100);
 }
 
 function hideOverlay() {

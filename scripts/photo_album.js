6c71317076310264009234db60f4025b50b32258
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-25 01:49:09 +0100

    Add files via upload
    Amélioration du code pour afficher les photos de l'album sans avoir à réécrire les sources

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -1,24 +1,25 @@
 const overlay = document.getElementById("overlay");
 const imageContainer = document.getElementById("image-container");
-const image = document.getElementById("image");
 const loading = document.getElementById("loading");
+const smallImage = document.querySelectorAll(".photos img");
+const fullImage = document.getElementById("image");
 
 //Fonction d'affichage de l'overlay et de la photo en grande taille//
-function openOverlay(url) {   
+function openOverlay(image) {   
     overlay.style.display = "block";
     imageContainer.style.display = "block";
     loading.style.display = "block";
 
-    image.src = url;
+    fullImage.src = image.src.replace("-sq.jpg", ".jpg");
 
     //Disparition du message de chargement dès que l'image est chargée//
-    image.onload = () => {
+    fullImage.onload = () => {
         loading.style.display = "none";
     }
     //Pour les appareils plus lents, vérification toutes les 0.1 secondes si l'image s'est chargée à plus de 45%//
     let checkInterval = setInterval(
         () => {
-            if (image.height >= image.height * 0.45){
+            if (fullImage.height >= fullImage.height * 0.45){
                 loading.style.display = "none";
                 clearInterval(checkInterval);
             } 
@@ -29,6 +30,7 @@ function openOverlay(url) {
 function hideOverlay() {
     overlay.style.display = "none";
     imageContainer.style.display = "none";
-    image.src = "";
+    fullImage.src = "";
     loading.style.display = "none";
 }
+

f8527ee308141b7b0220ee462a9f40b745d34d8a
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-26 13:57:32 +0100

    Add files via upload
    Ajout de transitions à l'ouverture et au défilement des photos

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -1,11 +1,17 @@
 const smallImage = document.querySelectorAll(".photos img");
 const fullImage = document.getElementById("image");
 const slides = document.querySelector(".slides");
+const imageContainer = document.getElementById("image-container");
+const overlay = document.getElementById("overlay");
 
 let currentImage = 0;
 
-function openOverlay(image) {   
+function openOverlay(image) {
     slides.style.display = "block";
+    setTimeout(() => {
+        imageContainer.style.opacity = 1;
+        overlay.style.opacity = 1;
+    }, 50);
 
     fullImage.src = image.src.replace("-sq.jpg", ".jpg");
     currentImage = Number(image.id);
@@ -13,17 +19,27 @@ function openOverlay(image) {
 
 function hideOverlay() {
     slides.style.display = "none";
+    setTimeout(() => {
+        imageContainer.style.opacity = 0;
+        overlay.style.opacity = 0;
+    }, 50);
     fullImage.src = "";
 }
 
 function nextImage() {
     currentImage = (currentImage + 1) % smallImage.length;
     image = smallImage[currentImage];
-    openOverlay(image);
+    imageContainer.style.opacity = 0;
+        setTimeout(() => {
+            openOverlay(image);
+        }, 300);
 }
 
 function previousImage() {
     currentImage = (currentImage - 1 + smallImage.length) % smallImage.length;
     image = smallImage[currentImage];
-    openOverlay(image);
+    imageContainer.style.opacity = 0;
+        setTimeout(() => {
+            openOverlay(image);
+        }, 300);
 }

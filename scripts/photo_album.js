b5aebe59c796a4ae85508d29d778f165683bcb82
AntoineSPR <158608284+AntoineSPR@users.noreply.github.com>
2024-03-26 12:46:33 +0100

    Add files via upload
    Simplification de l'affichage (section complète)

--- scripts/photo_album.js
+++ scripts/photo_album.js
@@ -1,33 +1,28 @@
-const overlay = document.getElementById("overlay");
-const imageContainer = document.getElementById("image-container");
-const loading = document.getElementById("loading");
 const smallImage = document.querySelectorAll(".photos img");
 const fullImage = document.getElementById("image");
+const slides = document.querySelector(".slides");
 
 let currentImage = 0;
 
 function openOverlay(image) {   
-    overlay.style.display = "block";
-    imageContainer.style.display = "block";
+    slides.style.display = "block";
 
     fullImage.src = image.src.replace("-sq.jpg", ".jpg");
     currentImage = Number(image.id);
 }
 
 function hideOverlay() {
-    overlay.style.display = "none";
+    slides.style.display = "none";
     fullImage.src = "";
 }
 
-function nextImage(event) {
-    event.stopPropagation();
+function nextImage() {
     currentImage = (currentImage + 1) % smallImage.length;
     image = smallImage[currentImage];
     openOverlay(image);
 }
 
-function previousImage(event) {
-    event.stopPropagation();
+function previousImage() {
     currentImage = (currentImage - 1 + smallImage.length) % smallImage.length;
     image = smallImage[currentImage];
     openOverlay(image);
